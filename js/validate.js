669d64f90d6be78609c980f1378521cb12cb35d7
Estebanmg58 <56441614+Estebanmg58@users.noreply.github.com>
2020-06-23 08:37:55 -0500

    Update validate.js

--- js/validate.js
+++ js/validate.js
@@ -1,15 +1,17 @@
 
 window.addEventListener('load',function(){
-
+    //validaciones de formularios con plugin
     $(document).ready(function(){
         $.validate({
             lang: 'es',
             errorMessagePosition: 'top',
             scrollToTopOnError: true
         });
+        //sacar calendario con plugin
         $("form input[name='fecha']").datepicker({
             dateFormat: 'yy-mm-dd'
         });
+        //sacar hora con plugin
         $("#hour").timepicker({
     
         });
